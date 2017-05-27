cbe6f8bbcca8a6eb76ecfe3153ab113d508b3e42
jiaokang <Administrator@HP201-1>
2017-05-27 15:33:21 +0800

    异常

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,6 +10,65 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+
+            /* string s = null;
+             if (s == null)
+             {
+                 throw new ArgumentNullException();
+             }*/
+
+            /*   try
+               {
+                   Person p = new Person();
+                   string s = null;
+                   string n = s.ToString();
+                   //p.test(s);
+               }
+               catch (Exception ex)
+               {
+                   //使用catch去查找系统异常类提供的信息
+                   Console.WriteLine(ex.Message);
+               }*/
+
+            //finally语句
+            /* int i = 3;
+             try
+             {
+
+                 string s = "abc";
+                 object obj = s;
+                 i = (int)obj;
+             }
+             catch (InvalidCastException ex)
+             {
+
+                 Console.WriteLine(ex.Message);
+             }
+             finally
+             {
+                 Console.WriteLine(i);
+             }*/
+            string[] name = { "张三", "李四", "王五" };
+            try 
+            {
+               
+
+                for (int i = 0; i < 6; i++)
+                {
+                    Console.WriteLine(name[i]);
+                }
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine(name[0]);
+            }
+
+
         }
     }
 }
